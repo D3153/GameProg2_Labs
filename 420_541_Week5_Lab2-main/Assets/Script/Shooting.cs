a5bb12ab8df638b96e89df2099c89c28ab097fcc
D3153 <82058076+D3153@users.noreply.github.com>
2023-02-27 19:56:01 -0500

    lab 2 week 5

--- 420_541_Week5_Lab2-main/Assets/Script/Shooting.cs
+++ 420_541_Week5_Lab2-main/Assets/Script/Shooting.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public GameManager gameManager;
+    public AudioSource audio;
     // Ray ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f, 0.5f,0));
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
@@ -17,16 +18,23 @@ public class Shooting : MonoBehaviour
     {
         if(Input.GetButton("Fire1")){
             Ray ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f, 0.5f,0));
+            ParticleSystem part = GetComponent<ParticleSystem>();
+            part.Play();
+            audio.Play();
             RaycastHit hitData;
-            gameManager = FindObjectOfType<GameManager>();
+            // gameManager = FindObjectOfType<GameManager>();
             if(Physics.Raycast(ray, out hitData))
             {
                 //target dissapear
-                gameObject.SetActive(false);
                 //game manager incease points
+                if(hitData.collider.tag =="Target"){
+                
+                gameManager = GameManager.Instance;
                 if(gameManager != null){
                     gameManager.IncrementScore();
+                    hitData.collider.gameObject.SetActive(false);
                 }
+            }
             }
         }
     }
